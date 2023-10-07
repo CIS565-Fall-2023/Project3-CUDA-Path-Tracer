0ab19531225f763fdbc7043df6d94892f0082fdb
plasmas <plasticplasmas@outlook.com>
2023-10-07 14:53:23 -0400

    [visual] Imperfect Specular (MIS)

--- src/interactions.h
+++ src/interactions.h
@@ -2,6 +2,8 @@
 
 #include "intersections.h"
 
+#define IMPERFECT_SPECULAR 1
+
 // CHECKITOUT
 /**
  * Computes a cosine-weighted random direction in a hemisphere.
@@ -40,6 +42,46 @@ glm::vec3 calculateRandomDirectionInHemisphere(
         + cos(around) * over * perpendicularDirection1
         + sin(around) * over * perpendicularDirection2;
 }
+
+#if IMPERFECT_SPECULAR == 1
+// generate perturbed specular sampling direction
+// GPU Gems 3 https://developer.nvidia.com/gpugems/gpugems3/part-iii-rendering/chapter-20-gpu-based-importance-sampling
+__host__ __device__ glm::vec3 calcPerturbedSpecularDirection(
+  glm::vec3 ray_in, // direction of incoming ray
+  glm::vec3 normal, // surface normal
+  thrust::default_random_engine& rng, // random engine
+  float alpha // Phone alpha
+) {
+  glm::vec3 world_dir(0.0f);
+  do {
+    thrust::uniform_real_distribution<float> u01(0, 1);
+    float xi1 = u01(rng);
+    float xi2 = u01(rng);
+
+    // 
+    float theta = acosf(powf(xi1, 1 / (alpha + 1.0f)));
+    float phi = 2.0f * PI * xi2;
+
+    // convert to cartesian coordinate where specular direction is the z-axis
+    glm::vec3 tangent_dir(cosf(phi) * sinf(theta), sinf(phi)/* * sinf(theta)*/, cosf(theta));
+  
+    // ideal specular direction
+    glm::vec3 specular_dir = glm::reflect(ray_in, normal);
+
+    // Create an orthogonal bases around specular_dir
+    glm::vec3 basis_z = specular_dir;
+    // find a direction that is not colinear with specular_dir, e.g. world up
+    glm::vec3 up = abs(basis_z.z) < 0.9999f ? glm::vec3(0, 0, 1) : glm::vec3(1, 0, 0);
+    glm::vec3 basis_x = glm::normalize(glm::cross(basis_z, up));
+    glm::vec3 basis_y = glm::cross(basis_z, basis_x);
+
+    // transform tangent_dir into world space using these bases
+    world_dir = glm::normalize(tangent_dir.x * basis_x + tangent_dir.y * basis_y + tangent_dir.z * basis_z);
+  } while (glm::dot(world_dir, normal) < 0);
+  return world_dir;
+}
+#endif // IMPERFECT_SPECULAR == 1
+
 
 /**
  * Scatter a ray with some probabilities according to the material properties.
@@ -88,19 +130,63 @@ void scatterRay(
     return;
   }
 
-  // PURE REFLECTIVE
+  // IMPERFECT SPECULAR (Phong Shading)
   if (m.hasReflective) {
+#if IMPERFECT_SPECULAR == 1
+    // sample both diffusive and specular directions
+    glm::vec3 ray_out_diffusive = calculateRandomDirectionInHemisphere(normal, rng);
+    float alpha = m.specular.exponent;
+    glm::vec3 ray_out_specular = calcPerturbedSpecularDirection(pathSegment.ray.direction, normal, rng, alpha);
+
+    // Calculate BSDF / PDF for both
+    float bsdf_diffuse = 1.0f / PI;
+    float pdf_diffuse = glm::dot(normal, ray_out_diffusive) / PI;
+
+    glm::vec3 specular_dir = glm::reflect(pathSegment.ray.direction, normal);
+    float cosTheta = glm::abs(glm::dot(specular_dir, ray_out_specular));
+    float bsdf_specular = (alpha + 2) / (2 * PI) * powf(cosTheta, alpha);
+    float pdf_specular = (alpha + 1) / (2 * PI) * powf(cosTheta, alpha);
+
+    // calculate MIS weights (power heuristic, beta = 2)
+    float mis_weight_sum = powf(pdf_diffuse, 2) + powf(pdf_specular, 2);
+    float weight_diffuse = powf(pdf_diffuse, 2) / mis_weight_sum;
+    float weight_specular = powf(pdf_specular, 2) / mis_weight_sum;
+
+    // calculate color multipliers
+    glm::vec3 color_diffuse = m.color * weight_diffuse * bsdf_diffuse / pdf_diffuse;
+    glm::vec3 color_specular = m.specular.color * weight_specular * bsdf_specular / pdf_specular;
+
+    // toss the dice
+    thrust::uniform_real_distribution<float> u01(0, 1);
+    float choice = u01(rng);
+
+    // impoerfect specular: diffusion + imperfect reflection (Phong)
+    if (choice < weight_diffuse) {
+      // diffuse branch taken
+      pathSegment.ray.direction = ray_out_diffusive;
+      pathSegment.ray.origin = intersect + ray_out_diffusive * 0.0001f;
+      // for lambertian diffusion, BSDF & PDF are aligned (no need to compute)
+      pathSegment.color *= color_diffuse;
+    }
+    else {
+      // specular branch taken
+      pathSegment.ray.direction = ray_out_specular;
+      pathSegment.ray.origin = intersect + ray_out_specular * 0.0001f;
+      pathSegment.color *= color_specular;
+    }
+#else
+    // original pure specular
     glm::vec3 ray_out = glm::reflect(pathSegment.ray.direction, normal);
     pathSegment.ray.direction = ray_out;
     pathSegment.ray.origin = intersect + ray_out * 0.0001f;
     pathSegment.color *= m.color;
+#endif
     return;
   }
 
   // PURE DIFFUSIVE
   // randomize a cosine-weighted ray out
   glm::vec3 ray_out = calculateRandomDirectionInHemisphere(normal, rng);
-
 
   // update pathSegment
   pathSegment.ray.direction = ray_out;
